0063c11584905eb70834d37c4f7b4a71aeae6127
Arya-chaurasia <aryachaurasia11@gmail.com>
2025-08-20 17:24:42 +0530

    config

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,7 @@ export const DESCRIPTION_OFFSET = 255;
 
 export const YOUTUBE_API_KEY = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics,liveStreamingDetails&chart=mostPopular&regionCode=IN&maxResults=50&key=" + GOOGLE_API_KEY;
 
